bc70c8827117dfd630a6f24dae9c2c9362832ebd
Gangbuster <earljg@gmail.com>
2018-03-03 11:11:32 -0600

    Final Crypto Commit
    This is a working Vigenere Cipher

--- vigenere.py
+++ vigenere.py
@@ -1,61 +1,29 @@
-def alphabet_position(letter):
-    alpha = "abcdefghijklmnopqrstuvwxyz"
-    alpha_upper = alpha.upper()
-    alpha_index = ""
-
-    for char in letter:
-        if char == " ":
-            alpha_index = alpha_index + " "
-        elif char not in alpha and char not in alpha_upper:
-            alpha_index = alpha_index + char
-        else:
-            if char in alpha:
-                alpha_index = alpha.index(char)
-            else:
-                alpha_index = alpha_upper.index(char)
-
-
-
-    return alpha_index
-
-def rotate_character(char, rot):
-    alpha_index = alphabet_position(char)
-    rotated = rot
-    alpha = "abcdefghijklmnopqrstuvwxyz"
-    alpha_upper = alpha.upper()
-    encrypted = ""
-
-    for i in char:
-        if i == " ":
-            encrypted = encrypted + " "
-        elif i not in alpha and i not in alpha_upper:
-            encrypted = encrypted + i
-
-        else:
-            rotated_index = alpha_index + rotated
-            if rotated_index < 26 and i in alpha:
-                encrypted = encrypted + alpha[rotated_index]
-            elif rotated_index < 26 and i in alpha_upper:
-                encrypted = encrypted + alpha_upper[rotated_index]
-            elif i in alpha:
-                encrypted = encrypted + alpha[rotated_index % 26]
-            else:
-                encrypted = encrypted + alpha_upper[rotated_index % 26]
-
-
-    return encrypted
+from helpers import alphabet_position, rotate_character
 
 def encrypt(text, rot):
-    rotated_index = 0
+    rotated_list = ""
+    encrypt_list = ""
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    alpha_upper = alpha.upper()
     final_encrypt = ""
+    m = len(rot)
+    n = 0
+    for i in range(len(text)):
+        if text[i] == " ":
+            n = n - 1
+            final_encrypt = final_encrypt + " "
 
-    for i in rot:
-        rotated_index = alphabet_position(i)
-    for char in text:
-        final_encrypt = final_encrypt + rotate_character(char, rotated_index)
+        elif text[i] not in alpha and text[i] not in alpha_upper:
+            n = n - 1
+            final_encrypt = final_encrypt + text[i]
+        #elif text[i - 1] == " ":
+            #rotated_list = alphabet_position(rot[(i - 1) % m])
+            #final_encrypt = final_encrypt + rotate_character(text[i], rotated_list)
+        else:
+            rotated_list = alphabet_position(rot[(i % m) + n])
+            final_encrypt = final_encrypt + rotate_character(text[i], rotated_list)
 
-
-    return rotated_index
+    return final_encrypt
 
 def main():
     #message = input("Type a message:")
